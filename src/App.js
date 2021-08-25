368915d2bec9ecf7a3992866aa2a5164f72b7d79
Rahul <bppit.11500218019@gmail.com>
2021-08-25 12:06:54 +0530

    created pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hi;
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          This is home page
+        </Route>
+
+        <Route exact path="/star">
+          This is starred page
+        </Route>
+
+        <Route>
+          This is 404 page. Page not found.
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
