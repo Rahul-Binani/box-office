608ffae01a41c4a0ac922a99e36535f6a7239c04
Rahul <bppit.11500218019@gmail.com>
2021-08-27 19:12:20 +0530

    added radios and functionalit for searching

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
+  const [results, setResults] = useState(null); 
   const [input, setInput] = useState('');
+  const [searchOption, setSearchOption] = useState('shows');
+  const isShowSearch = searchOption === 'shows';
 
   const onSearch = () => {
+
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+
     fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
       .then(r => r.json())
       .then(result => {
-        console.log(result);
+        setResults(result);
       });
   };
 
@@ -22,17 +31,51 @@ const Home = () => {
     }
   };
 
+  const onRadioChange = ev => {
+    setSearchOption( ev.target.value )
+  }
+
+  const renderResults = () => {
+    if(results && results.length === 0){
+        return <div>No result</div>
+    }
+    if(results && results.length>0)
+    {
+        return results[0].show ? results.map(item => (
+          <div key = {item.show.id}>{item.show.name}</div>
+        )) : results.map(item => (
+          <div key = {item.person.id}>{item.person.name}</div>))
+    };
+    return null;
+  }
+
   return (
     <MainPageLayout>
       <input
         type="text"
+        placeholder = "Search for something"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
+      
+    <div>
+      <label htmlFor = "show">
+        Shows
+        <input id = "show" type = "radio" value = "shows" onChange = {onRadioChange} checked = {isShowSearch} />
+      </label>
+
+      <label htmlFor = "actor-search">
+        Actors
+        <input id = "actor-search" type = "radio" value = "people" onChange={onRadioChange} checked = {!isShowSearch} />
+      </label>
+
+    </div>
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
