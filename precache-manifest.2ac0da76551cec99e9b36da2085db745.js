e663a2c6f361caf83be29bc275afe48eb38ebd85
Rahul <bppit.11500218019@gmail.com>
2021-08-29 13:21:41 +0530

    Updates

---
+++ precache-manifest.2ac0da76551cec99e9b36da2085db745.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539248f4d571a5ada89c73ba91a70131",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "45c7a7e9752a580289a6",
+    "url": "/box-office/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "6ed47fe6a9d5dd48217d",
+    "url": "/box-office/static/js/2.8baaf4c0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.8baaf4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96085cdb683172e2fba",
+    "url": "/box-office/static/js/3.e8622963.chunk.js"
+  },
+  {
+    "revision": "45c7a7e9752a580289a6",
+    "url": "/box-office/static/js/main.21b42de4.chunk.js"
+  },
+  {
+    "revision": "51050238b30855c191f7",
+    "url": "/box-office/static/js/runtime-main.a404571e.js"
+  }
+]);
